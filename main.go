3607761e5c6c6cc0c0a3b141364ce95a0d649c70
ElenaGavrina <45360118+ElenaGavrina@users.noreply.github.com>
2023-06-15 12:13:34 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"path/filepath"
 
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "ok")
-	})
-	return r
-}
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 func main() {
-	r := setupRouter()
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ok",
+		})
+	})
+
+	r.POST("/upload", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			return
+		}
+
+		filename, err := uuid.Parse(filepath.Base(file.Filename))
+		if err != nil {
+			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"fileName": filename,
+		})
+	})
+	
 	r.Run(":8080")
 }
